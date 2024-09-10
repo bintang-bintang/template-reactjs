50534b250975486b98399bc1e162d0ab698b42a5
bintang-bintang <bintangajiwibowo@gmail.com>
2024-09-10 07:06:35 +0700

    Penambahan folder pages dan components dan image dummy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
